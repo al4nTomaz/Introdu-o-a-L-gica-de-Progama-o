64437f8d319104ffc4eee57b683c4a6d77aa0aa5
al4nTomaz <alantomazifprpgua@gmail.com>
2023-04-11 10:54:35 -0300

    prova e exercicio da faturação do número de 3 digitos

---
+++ aulas/aula1104/faturando.java
@@ -0,0 +1,28 @@
+package aulas.aula1104;
+
+import java.util.Scanner;
+
+public class faturando {
+
+    public static void main(String[] args) {
+
+        int num, soma = 0, aux=0;
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Digite um número de 100 a 999: ");
+        num = entrada.nextInt(); 
+
+
+        soma = num  / 100; 
+        aux = num  % 100; 
+        soma += aux  / 10;
+        soma += aux  % 10;
+        
+        System.out.println("\nO resultado é: "+soma);
+        
+        entrada.close();
+
+        
+    }
+} 
