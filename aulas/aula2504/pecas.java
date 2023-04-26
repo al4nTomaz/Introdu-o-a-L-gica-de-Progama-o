696d002723af800cdad673080944adc6a9e80fff
al4nTomaz <alantomazifprpgua@gmail.com>
2023-04-26 10:12:32 -0300

    organização das pastas

--- aulas/aula2504/pecas.java
+++ aulas/aula2504/pecas.java
@@ -2,7 +2,17 @@ package aulas.aula2504;
 
 public class pecas {
     public static void main(String[] args) {
-        char peca = 11035;
+
+        int i = 0;
+        char peca = (char) i;
+
+        for (;i < 1000; i++) {
+            System.out.println(i+"\t"+peca);
+            peca = (char) i;    
+        }
+
+        peca = 18;
+        
 
         System.out.print(peca);
         System.out.print(peca+"\n");
